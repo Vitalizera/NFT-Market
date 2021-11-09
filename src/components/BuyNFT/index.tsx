f026c3515174f60325d6fe32c0daafd43ff3cebb
Lucas de Oliveira <lucasoliveira13670@gmail.com>
2021-11-09 15:50:48 -0400

    new items

--- src/components/BuyNFT/index.tsx
+++ src/components/BuyNFT/index.tsx
@@ -3,15 +3,20 @@ import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
 import busd_abi from '../../busd_abi.json';
 import { Container, NFTCard } from './styles';
-import MisteryBoxImage from "../assets/D_NQ_NP_685253-MLB46765702054_072021-O.jpg";
+import MisteryBoxImage from '../../assets/D_NQ_NP_685253-MLB46765702054_072021-O.jpg';
 
+interface BuyNFTProps {
+  id: string;
+  name: string;
+  price: string;
+  busd_real_price: string;
+}
 const web3 = new Web3(Web3.givenProvider);
 
 const busdContractAddress = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
-const totalToTransfer = '50000000000000000000';
+// // const totalToTransfer = '50000000000000000000';
 
-export function BuyNFT() {
-
+export function BuyNFT({ name, price, id, busd_real_price }: BuyNFTProps) {
   const ethEnabled = async () => {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
@@ -26,7 +31,7 @@ export function BuyNFT() {
         await busdContract.methods
           .transfer(
             '0x4C486eD00E15e498640512c15Ccd9e06B3682Bd6',
-            totalToTransfer
+            busd_real_price
           )
           .send({
             from: await metamaskAccount.result[0],
@@ -37,12 +42,12 @@ export function BuyNFT() {
     }
   };
   return (
-    <Container>
+    <Container id={id}>
       <NFTCard>
-        <img src={MisteryBoxImage} alt='mistery_box' />
-        <h1>Mistery Box</h1>
+        <img src={MisteryBoxImage} alt="mistery_box" />
+        <h1>{name}</h1>
       </NFTCard>
-      <button onClick={ethEnabled}>Price: 50 BUSD</button>
+      <button onClick={ethEnabled}>Price: {price}</button>
     </Container>
   );
 }
--- src/components/LandingPageLayout/index.tsx
+++ src/components/LandingPageLayout/index.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BuyNFT } from '../BuyNFT';
 
 import { Container } from './styles';
 
+interface NftItemsProps {
+  id: number;
+  name: string;
+  url_image?: string;
+  price: string;
+  busd_real_price: string;
+}
+const items = [
+  {
+    id: 0,
+    name: 'Mistery Box 1',
+    price: '50 BUSD',
+    busd_real_price: '50000000000000000000',
+  },
+  {
+    id: 1,
+    name: 'Mistery Box 2',
+    price: '60 BUSD',
+    busd_real_price: '60000000000000000000',
+  },
+  {
+    id: 2,
+    name: 'Mistery Box 3',
+    price: '70 BUSD',
+    busd_real_price: '70000000000000000000',
+  },
+];
+
 export function LandingPageLayout() {
+  const [nftItems, setNftItems] = useState<NftItemsProps[]>([]);
+  useEffect(() => {
+    setNftItems(items);
+  }, []);
   return (
     <Container>
-      <BuyNFT />
+      {nftItems.map((item) => (
+        <BuyNFT
+          id={item.id.toString()}
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          busd_real_price={item.busd_real_price}
+        />
+      ))}
     </Container>
   );
 }
